1bbacd1655c4e7b0b26c83385dd3f86003b08301
paulstafki <waynesworldwails@hotmail.com>
2015-09-14 10:45:05 -0500

    Seperated out indexhtml controller

--- server/public/assets/scripts/controllers/HomeController.js
+++ server/public/assets/scripts/controllers/HomeController.js
@@ -1,16 +1,6 @@
 /**
  * Created by lukedowell on 9/9/15.
  */
-app.controller("HomeController", function($scope, $timeout, $mdSidenav, $mdUtil) {
-    $scope.toggleLeft = buildToggler('left');
-    function buildToggler(navID) {
-        var debounceFn =  $mdUtil.debounce(function(){
-            $mdSidenav(navID)
-                .toggle();
-        },200);
-        return debounceFn;
-    }
-    $scope.close = function () {
-        $mdSidenav('left').close();
-    };
+app.controller("HomeController", function($scope) {
+
 });
---
+++ server/public/assets/scripts/controllers/IndexController.js
@@ -0,0 +1,14 @@
+//Index controller, controls left navbar --PR 9-14-15
+app.controller("IndexController", function($scope, $timeout, $mdSidenav, $mdUtil) {
+    $scope.toggleLeft = buildToggler('left');
+    function buildToggler(navID) {
+        var debounceFn =  $mdUtil.debounce(function(){
+            $mdSidenav(navID)
+                .toggle();
+        },200);
+        return debounceFn;
+    }
+    $scope.close = function () {
+        $mdSidenav('left').close();
+    };
+});
