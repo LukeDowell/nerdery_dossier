756afe53a16cf9ca3432ceac2c9a40ca0b87c67e
Luke Dowell <lukedowell@gmail.com>
2015-09-16 15:26:03 -0500

    Because you can never have a bug free merge

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,6 @@ var path = require('path');
  */
 router.get('/*', function(req, res) {
     var file = req.params[0] || "assets/views/index.html";
-    console.log(file);
     res.sendFile(path.join(__dirname, "../public", file));
 });
 
